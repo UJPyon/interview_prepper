cb977947461b520a4112983c4923e16e59a59f1c
Jae <ujpyon@gmail.com>
2019-12-13 15:29:36 -0500

    Add user model, route and controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,33 @@
+class Api::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user) 
+      render 'api/users/show'
+    else
+      render json: @user.errors.full_messages, status: 401
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render 'api/users/show'
+    else
+      render json: @user.errors.full_messages, status: 404
+    end
+  end
+
+  def index
+    @users = User.all
+    render 'api/users/index'
+  end
+
+  private 
+  
+  def user_params
+    params.require(:user).permit(:email, :fname, :lname, :password, :text) 
+  end
+
+end
