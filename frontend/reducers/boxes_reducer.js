c9ca56df196deb66ec5f57d199b303e53a56bdec
Jae <ujpyon@gmail.com>
2019-12-18 15:50:06 -0500

    Create boxes reducer

---
+++ frontend/reducers/boxes_reducer.js
@@ -0,0 +1,26 @@
+import {
+  FETCH_BOX,
+  FETCH_ALL_BOXES,
+  DELETE_BOX
+} from "../actions/box_actions";
+import { merge } from "lodash";
+
+const boxesReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let newState;
+  
+  switch (action.type) {
+    case FETCH_BOX:
+      return merge({}, state, { [action.box.id]: action.box });
+    case FETCH_ALL_BOXES:
+      return merge({}, action.boxes);
+    case DELETE_BOX:
+      newState = merge({}, state);
+      delete newState[action.id];
+      return newState;
+    default:
+      return state;
+  }
+}
+
+export default boxesReducer;
