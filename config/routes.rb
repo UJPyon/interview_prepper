2701f1d0bc2d5c200b4facfc7a63db3b34304c95
Jae <ujpyon@gmail.com>
2019-12-18 11:11:42 -0500

    Add submission namespace api routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
-    
+    resources :submissions, only: [:show, :index, :create, :update, :destroy]
   end
 
 end
