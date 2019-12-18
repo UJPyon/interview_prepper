4e706a85b43a17947d58ebfd38d0edabdbfe8e55
Jae <ujpyon@gmail.com>
2019-12-18 11:09:34 -0500

    Add restful routing methods to submissions controller

--- app/controllers/api/submissions_controller.rb
+++ app/controllers/api/submissions_controller.rb
@@ -1,30 +1,36 @@
 class Api::SubmissionsController < ApplicationController
 
   def index
-
+    @submissions = Submission.all
+    render :index
   end
-
+  
   def show
-
-  end
-
-  def new
-
+    @submission = Submission.find(params[:id])
+    render :show
   end
 
   def create
     @submission = Submission.new(submission_params)
     if @submission.save
-      
+      render :show
+    else
+      render json: @submission.errors.full_messages, status: 422
     end
   end
 
   def update
-
+    @submission = Submission.find(params[:id])
+    if @submission.update(submission_params)
+      render :show
+    else
+      render json: @submission.errors.full_messages, status: 422
   end
 
   def destroy
-
+    @submission = Submission.find(params[:id])
+    @submission.destroy
+    render :index
   end
 
   private
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,7 +4,7 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login!(@user) 
-      render 'api/users/show'
+      render :show
     else
       errors = @user.errors.full_messages.map do |error|
         if error == "Username can't be blank"
