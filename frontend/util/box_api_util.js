a35f689e4945384bcf14c0f0b5a1a9215a3e889a
Jae <ujpyon@gmail.com>
2019-12-18 15:06:50 -0500

    Create box util file

---
+++ frontend/util/box_api_util.js
@@ -0,0 +1,36 @@
+export const getAllBoxes = () => {
+  return $.ajax({
+    method: "GET",
+    url: "api/boxes",
+  });
+};
+
+export const findBox = id => {
+  return $.ajax({
+    method: "GET",
+    url: `api/box/${id}`,
+  });
+};
+
+export const createBox = box => {
+  return $.ajax({
+    method: "PATCH",
+    url: "api/boxes",
+    data: { box },
+  });
+};
+
+export const updateBox = box => {
+  return $.ajax({
+    method: "PATCH",
+    url: `api/box/${box.id}`,
+    data: { box },
+  });
+};
+
+export const deleteBox = id => {
+  return $.ajax({
+    method: "DELETE",
+    url: `api/box/${id}`
+  });
+};
--- frontend/util/submission_api_util.js
+++ frontend/util/submission_api_util.js
@@ -1,27 +1,36 @@
 export const getAllSubmissions = () => {
   return $.ajax({
     method: "GET",
-    url: "api/submissions"
+    url: "api/submissions",
   });
 };
 
 export const findSubmission = id => {
   return $.ajax({
     method: "GET",
-    url: `api/submission/${id}`
+    url: `api/submission/${id}`,
   });
 };
 
-export const updateSubmission = id => {
+export const createSubmission = sub => {
+  return $.ajax({
+    method: "POST",
+    url: "api/submissions",
+    data: { sub },
+  });
+};
+
+export const updateSubmission = sub => {
   return $.ajax({
     method: "PATCH",
-    url: `api/submission/${id}`
+    url: `api/submission/${sub.id}`,
+    data: { sub },
   });
 };
 
 export const deleteSubmission = id => {
   return $.ajax({
     method: "DELETE",
-    url: `api/submission/${id}`
+    url: `api/submission/${id}`,
   });
 };
